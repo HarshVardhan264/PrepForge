47931771a5c3473c7050707148f42050f0c74153
HarshVardhan264 <harshvar2642002@gmail.com>
2026-08-21 15:08:03 +0530

    bug fix

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,22 +1,27 @@
 const express = require('express');
-const cookieParser = require('cookie-parser')
-const cors = require('cors')
-/*
- * require all the routes here
- */
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
 const authRouter = require('./routes/auth.routes');
 const interviewRouter = require('./routes/interview.routes');
 
-
-
 const app = express();
-app.use(cookieParser())
+
+app.use(cookieParser());
+
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
-}))
+    origin: (origin, callback) => {
+        if (!origin || /^http:\/\/localhost:\d+$/.test(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true
+}));
 
 app.use(express.json());
+
 app.use('/api/auth', authRouter);
 app.use('/api/interview', interviewRouter);
 
